b153db2633f6444ced4db1b0388c243a23112c50
Aaron Friedman <dubalator@gmail.com>
2018-03-02 03:09:21 -1000

    Setup basscss.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        loader: ['style-loader', 'css-loader', 'postcss-loader']
+
       },
       {
         test: /\.(png|jpg)$/,
